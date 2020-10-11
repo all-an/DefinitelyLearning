92231e5a3ef250f1267931c9f16e762234618e55
all-an <all-an@hotmail.com>
2020-10-11 12:17:16 -0300

    origin master

--- Playin With Sprites/Text101/Assets/AdventureGame.cs
+++ Playin With Sprites/Text101/Assets/AdventureGame.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] Text textComponent;
+
     void Start()
     {
-        
+        textComponent.text = ("When walking in the road, you see a chest in the middle of the road. " +
+            "You open the chest or you keep going ?");
     }
 
     // Update is called once per frame
