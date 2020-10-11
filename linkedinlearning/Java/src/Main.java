b636a733ab1110c66cec774724e0222e29caa71e
all-an <all-an@hotmail.com>
2020-10-11 16:25:08 -0300

    learning

--- linkedinlearning/Java/src/Main.java
+++ linkedinlearning/Java/src/Main.java
@@ -1,20 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int studentAge =  15;
-        double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
+        }
+        /*int studentAge =  15;
+        double studentGPA;
+
         boolean hasPerfectAttendance = true;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
 
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+
         System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
+        //System.out.println(studentGPA);
+
         System.out.println(hasPerfectAttendance);
         System.out.println(studentFirstName);
         System.out.println(studentLastName);
-            //System.out.println("Hello World from the intellij ide");
-    }
+
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        //System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to ?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
+        //System.out.println("Hello World from the intellij ide");*/
 }
