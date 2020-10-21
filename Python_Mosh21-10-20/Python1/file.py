272c9741f9a0c10862a7c90af0121c78dda8ad5f
all-an <all-an@hotmail.com>
2020-10-21 12:46:15 -0300

    learning

---
+++ Python_Mosh21-10-20/Python1/file.py
@@ -0,0 +1,14 @@
+age = 20
+age = 19.95
+first_name = "Mosh"
+is_online = False
+print("Hello World")
+print(age)
+
+name = "John Smith"
+patient_age = 20
+new_patient = True
+
+print("Patient name: ", name)
+print("Patient age: ", patient_age)
+print("Patient is new here: ", new_patient)
